b9a55f05560ceb6985244a99d9ca675ff1cf78b3
Vladislav Lukin <vladislavlukin090392@gmail.com>
2025-02-08 20:44:35 +0400

    update class Product

--- src/product.py
+++ src/product.py
@@ -5,8 +5,13 @@ class Product:
     price: float
     quantity: int
 
+    # # Атрибут, суммирующий общее количество товаров
+    total_quantity = 0
+
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
+
+        Product.total_quantity += self.quantity
