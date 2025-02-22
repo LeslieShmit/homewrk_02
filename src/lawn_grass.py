0d42feb41b97ee7586b0dcbb985f9d8710bf9e4d
Vladislav Lukin <vladislavlukin090392@gmail.com>
2025-02-22 18:38:26 +0400

    fixes

--- src/lawn_grass.py
+++ src/lawn_grass.py
@@ -9,6 +9,7 @@ class LawnGrass(Product):
 
     def __init__(self, name, description, price, quantity, country, germination_period, color):
         super().__init__(name, description, price, quantity)
+        self.__price = price
         self.country = country
         self.germination_period = germination_period
         self.color = color
--- src/smartphone.py
+++ src/smartphone.py
@@ -10,6 +10,7 @@ class Smartphone(Product):
 
     def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
         super().__init__(name, description, price, quantity)
+        self.__price = price
         self.efficiency = efficiency
         self.model = model
         self.memory = memory
