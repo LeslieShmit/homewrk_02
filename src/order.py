bef9725895449fc0e5f8deb45c3e210d3d3be4cf
Vladislav Lukin <vladislavlukin090392@gmail.com>
2025-02-25 23:51:04 +0400

    add class Order

---
+++ src/order.py
@@ -0,0 +1,16 @@
+from src.product import Product
+from src.abstract_group import AbstractGroup
+
+
+class Order(AbstractGroup):
+    product: Product
+    quantity: int
+    total_price: float
+
+    def __init__(self, product, quantity):
+        self.product = product
+        self.quantity = quantity
+        self.total_price = product.price * quantity
+
+    def __str__(self):
+        return f"Заказанный товар: {self.product}, кол-во: {self.quantity}, общая стоимость: {self.total_price}"
