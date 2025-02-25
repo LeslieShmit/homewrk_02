3ca68e9b74cb875d37fb5d3473c035a27b98fc54
Vladislav Lukin <vladislavlukin090392@gmail.com>
2025-02-25 22:51:27 +0400

    add class BaseProduct

---
+++ src/base_product.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+
+class BaseProduct(ABC):
+    """Абстрактный базовый класс для представления продуктов"""
+    @classmethod
+    @abstractmethod
+    def new_product(cls, *args, **kwargs):
+        pass
